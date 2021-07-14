2e52ac063528b77c26f8e838eff4bdcd3420dc6c
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-07-14 23:01:46 +0200

    [UPDATE] sekelton anim

--- Assets/Scripts/SkeletonCharacter.cs
+++ Assets/Scripts/SkeletonCharacter.cs
@@ -11,12 +11,24 @@ public class SkeletonCharacter : MonoBehaviour
 
     Transform transform;
 
+    AudioSource audioSourceSwordAttack1;
+
+    AudioClip audioClipAttack1;
+    
+
     float skeletonSpeed = 4.0f;
 
     /**
         Make sure the skeleton don't collide with the player (hitbox)
     */
-    float safeXDistanceForMove = 0.5f;
+    float safeXDistanceForMove = 0.7f;
+
+    /**
+        Hitbox to detect player is enough close to trigger attack
+    **/
+    float hitboxRadiusX = 1f;
+    float hitboxRadiusY = 1f;
+    float hitboxRadiusZ = 1f;
 
 
     // Start is called before the first frame update
@@ -25,6 +37,8 @@ public class SkeletonCharacter : MonoBehaviour
         this.sphereCollider = gameObject.GetComponent<SphereCollider>();
         this.animator = gameObject.GetComponent<Animator>();
         this.transform = gameObject.GetComponent<Transform>();
+        this.audioSourceSwordAttack1 = gameObject.GetComponent<AudioSource>();
+        this.audioClipAttack1 = this.audioSourceSwordAttack1.clip;
     }
 
     // Update is called once per frame
@@ -32,7 +46,7 @@ public class SkeletonCharacter : MonoBehaviour
     {
         if ( isAggro == true ) {
             Debug.Log("SKELETON AGGROED");
-            
+
             transform.LookAt(this.hitterGameObject.transform.position);
             this.animator.SetBool("isAggroStartRun", isAggro);
             this.animator.SetBool("isAggroStopRun", isAggro);
@@ -40,6 +54,9 @@ public class SkeletonCharacter : MonoBehaviour
             float step =  this.skeletonSpeed * Time.deltaTime; // calculate distance to move
             Vector3 vecCurrentPlayerPosWithSafeSpace = new Vector3(this.hitterGameObject.gameObject.transform.position.x - this.safeXDistanceForMove, this.hitterGameObject.gameObject.transform.position.y, this.hitterGameObject.gameObject.transform.position.z);
             transform.position = Vector3.MoveTowards(transform.position, vecCurrentPlayerPosWithSafeSpace, step);
+
+            triggeringAttack(this.hitterGameObject.gameObject.transform.position, transform.position);
+
         }
     }
 
@@ -69,5 +86,21 @@ public class SkeletonCharacter : MonoBehaviour
             this.animator.SetBool("isAggroStopRun", isAggro);
             this.hitterGameObject = null; // reset hitted game object
         }
+    }
+
+    void triggeringAttack (Vector3 playerPosition, Vector3 skeletonPosition) {
+            // Compute radius 
+            if ( 
+                playerPosition.x >= skeletonPosition.x &&
+                playerPosition.x <= skeletonPosition.x + this.hitboxRadiusX ) 
+                {
+
+                    if ( !this.audioSourceSwordAttack1.isPlaying ) {
+                        this.audioSourceSwordAttack1.PlayOneShot(this.audioClipAttack1, 0.7f);
+                    }
+                    this.animator.SetBool("isAttacking1", true);
+                } else {
+                    this.animator.SetBool("isAttacking1", false);
+                }
     }
 }
